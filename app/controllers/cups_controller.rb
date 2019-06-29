3be92c26caf9c4af30928cb638de4798c216d3bf
iamtash <natashachernis@gmail.com>
2019-06-28 19:47:10 -0500

    new cups action

--- app/controllers/cups_controller.rb
+++ app/controllers/cups_controller.rb
@@ -8,4 +8,17 @@ class CupsController < ApplicationController
             redirect '/login'
         end
     end
+
+    get '/cups/new' do 
+        if logged_in?
+            @user = current_user
+            erb :'/cups/new'
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/cups' do 
+        cup = Cup.create
+    end
 end
