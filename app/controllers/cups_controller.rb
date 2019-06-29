f1ceb4f4a347ba5cce0c3fdc3bad466f0682db6b
github username <github email address>
2019-06-28 23:21:30 -0500

    build new cup form

--- app/controllers/cups_controller.rb
+++ app/controllers/cups_controller.rb
@@ -1,24 +1,28 @@
 class CupsController < ApplicationController
-    get '/cups' do 
+    get '/cups' do
         if logged_in?
             @user = current_user
             @cups = Cup.all
             erb :'/cups/index'
-        else   
+        else
             redirect '/login'
         end
     end
 
-    get '/cups/new' do 
+    get '/cups/new' do
         if logged_in?
-            @user = current_user
+            @roasts = Coffee.roasts
+            @roasters = Roaster.all
             erb :'/cups/new'
         else
             redirect '/login'
         end
     end
 
-    post '/cups' do 
-        cup = Cup.create
+    post '/cups' do
+        coffee = Coffee.find_or_create_by(params[:coffee])
+        binding.pry
+        cup = coffee.cups.create(params[:cup])
+
     end
 end
--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -1,5 +1,11 @@
 class Coffee < ActiveRecord::Base
     belongs_to :roaster
-    has_many :cups 
-    has_many :users, through: :cups 
+    has_many :cups
+    has_many :users, through: :cups
+
+    ROASTS = ['light', 'medium', 'dark']
+
+    def self.roasts
+      ROASTS
+    end
 end
