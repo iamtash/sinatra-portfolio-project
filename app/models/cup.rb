fc89f8e92bc36b555f36cecb86ef948505d67c09
iamtash <natashachernis@gmail.com>
2019-06-30 23:33:54 -0500

    cup validation, parse timestamp

--- app/models/cup.rb
+++ app/models/cup.rb
@@ -1,4 +1,11 @@
 class Cup < ActiveRecord::Base
     belongs_to :user 
     belongs_to :coffee
+
+    validates :name, :brew, presence: true
+
+    def sip_time 
+        self.created_at.strftime("%A, %B %-d, %Y at %l:%M%P UTC")
+    end
+
 end
