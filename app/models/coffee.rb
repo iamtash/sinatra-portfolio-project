62d92f632182fec33a77597038e2ef8f8bba4f31
iamtash <natashachernis@gmail.com>
2019-06-30 23:33:09 -0500

    add coffee slug methods

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -3,9 +3,19 @@ class Coffee < ActiveRecord::Base
     has_many :cups
     has_many :users, through: :cups
 
+    validates :name, :roast, :roaster, presence: true
+
     ROASTS = ['light', 'medium', 'dark']
 
     def self.roasts
       ROASTS
+    end
+
+    def slug
+      self.name.downcase.strip.gsub(' ', '-').gsub('&', 'and').gsub(/[^\w-]/, '')
+    end
+  
+    def self.find_by_slug(slug)
+      self.all.find {|coffee| coffee.slug == slug}
     end
 end
