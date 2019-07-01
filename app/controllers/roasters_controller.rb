72d7ddcfdadaac4898f7bc48a77ee2e01cd02897
iamtash <natashachernis@gmail.com>
2019-06-30 23:32:49 -0500

    build Roasters controller routes

--- app/controllers/roasters_controller.rb
+++ app/controllers/roasters_controller.rb
@@ -1,5 +1,31 @@
-class CupsController < ApplicationController
+class RoastersController < ApplicationController
     get '/roasters' do 
-        erb :'/roasters/index'
+        if logged_in?
+            @roasters = Roaster.all
+            erb :'/roasters/index'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/roasters/new' do 
+        if logged_in?
+            erb :'/roasters/new'
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/roasters' do
+        
+    end
+
+    get '/roasters/:slug' do 
+        if logged_in?
+            @roaster = Roaster.find_by_slug(params[:slug])
+            erb :'/roasters/show'
+        else
+            redirect '/login'
+        end 
     end
 end
