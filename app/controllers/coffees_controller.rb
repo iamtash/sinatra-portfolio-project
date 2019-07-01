438c1a8f5d67d482e3ac3e25748876e62f7fc42a
iamtash <natashachernis@gmail.com>
2019-06-30 23:31:33 -0500

    build coffees controller routes

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,5 +1,39 @@
-class CupsController < ApplicationController
+class CoffeesController < ApplicationController
     get '/coffees' do 
-        erb :'/coffees/index'
+        if logged_in?
+            @coffees = Coffee.all
+            erb :'/coffees/index'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/coffees/new' do 
+        if logged_in?
+            @roasts = Coffee.roasts
+            @roasters = Roaster.all
+            erb :'/coffees/new'
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/coffees' do 
+        @coffee = Coffee.find_or_create_by(params[:coffee])
+
+        if params[:roaster][:name] != ""
+            @coffee.update(roaster: Roaster.find_or_create_by(params[:roaster]))
+        end
+
+        redirect "/coffees/#{@coffee.slug}"
+    end
+
+    get '/coffees/:slug' do 
+        if logged_in?
+            @coffee = Coffee.find_by_slug(params[:slug])
+            erb :'/coffees/show'
+        else
+            redirect '/login'
+        end
     end
 end
