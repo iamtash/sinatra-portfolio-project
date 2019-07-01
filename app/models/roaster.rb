befc37c7aa153d852a9afb5f4a172afb9d652bfc
iamtash <natashachernis@gmail.com>
2019-06-30 23:34:12 -0500

    add roaster validation and slug methods

--- app/models/roaster.rb
+++ app/models/roaster.rb
@@ -1,3 +1,13 @@
 class Roaster < ActiveRecord::Base
     has_many :coffees
+
+    validates :name, uniqueness: { case_sensitive: false }
+
+    def slug
+        self.name.downcase.strip.gsub(' ', '-').gsub('&', 'and').gsub(/[^\w-]/, '')
+      end
+    
+      def self.find_by_slug(slug)
+        self.all.find {|roaster| roaster.slug == slug}
+      end
 end
