027f43fda741f0e8e8161b9bb471957d25e2fb9c
iamtash <natashachernis@gmail.com>
2019-06-30 19:11:45 -0500

    add cup editing action

--- app/controllers/cups_controller.rb
+++ app/controllers/cups_controller.rb
@@ -31,4 +31,34 @@ class CupsController < ApplicationController
 
         erb :'cups/show'
     end
+
+    get '/cups/:id/edit' do
+        if logged_in? 
+            if @cup = Cup.find_by(id: params[:id])
+                @roasts = Coffee.roasts
+                @roasters = Roaster.all
+                erb :'/cups/edit'
+            else
+                redirect '/cups'
+            end
+        else
+            redirect '/login'
+        end
+    end
+
+    patch '/cups/:id' do
+        @cup = Cup.find_by(id: params[:id])
+
+        @cup.update(params[:cup])
+
+        @coffee = Coffee.find_or_create_by(params[:coffee])
+
+        if params[:roaster][:name] != ""
+            @coffee.roaster = Roaster.create(params[:roaster])
+        end
+
+        @cup.update(coffee: @coffee)
+
+        erb :'cups/show'
+    end
 end
