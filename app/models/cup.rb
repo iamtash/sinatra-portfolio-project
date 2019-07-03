de054d6a832f6be3b86e5a4bcc3513767456c727
iamtash <natashachernis@gmail.com>
2019-07-03 00:01:24 -0500

    add simplified date

--- app/models/cup.rb
+++ app/models/cup.rb
@@ -2,10 +2,14 @@ class Cup < ActiveRecord::Base
     belongs_to :user 
     belongs_to :coffee
 
-    validates :name, :brew, presence: true
+    validates :brew, :coffee, :user, presence: true
 
     def sip_time 
         self.created_at.strftime("%A, %B %-d, %Y at %l:%M%P UTC")
+    end
+
+    def sip_date
+        self.created_at.strftime("%A, %B %-d, %Y")
     end
 
 end
