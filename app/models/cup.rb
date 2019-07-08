b25540100f33ffcc8988aaff31f072ace0c31eef
iamtash <natashachernis@gmail.com>
2019-07-07 23:01:54 -0500

    normalize user input for coffee brew

--- app/models/cup.rb
+++ app/models/cup.rb
@@ -1,7 +1,7 @@
 class Cup < ActiveRecord::Base
     belongs_to :user
     belongs_to :coffee
-
+    before_validation :normalize_brew
     validates :brew, :coffee, :user, presence: true
     validates_associated :coffee
 
@@ -16,5 +16,10 @@ class Cup < ActiveRecord::Base
     def a_or_an
         brew.start_with?(/[aeiou]/) ? 'an' : 'a'
     end
+
+    private
+        def normalize_brew
+            self.brew = brew.split(' ').map {|w| w.downcase}.join(' ')
+        end
 
 end
