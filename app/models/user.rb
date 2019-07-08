20bccb24afcff7d6ba4fa2843465207dbbe1bd5c
iamtash <natashachernis@gmail.com>
2019-07-07 23:02:29 -0500

    normalize user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,20 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :cups 
     has_many :coffees, through: :cups
+    before_validation :normalize_name
     validates :email, {presence: true, uniqueness: { case_sensitive: false }}
     validates :name, :password, presence: true
 
     def slug
         self.name.downcase.strip.gsub(' ', '-').gsub('&', 'and').gsub(/[^\w-]/, '')
-      end
+    end
     
-      def self.find_by_slug(slug)
+    def self.find_by_slug(slug)
         self.all.find {|user| user.slug == slug}
-      end
+    end
+
+    private
+        def normalize_name
+            self.name = name.split(' ').map {|w| w.downcase.capitalize}.join(' ')
+        end
 end
