daa086ac0ea4cd2e35aa7310635572174317d073
iamtash <natashachernis@gmail.com>
2019-07-09 00:34:23 -0500

    add option to add new coffee to existing roaster, add success flash msgs

--- app/controllers/roasters_controller.rb
+++ app/controllers/roasters_controller.rb
@@ -17,13 +17,28 @@ class RoastersController < ApplicationController
     end
 
     post '/roasters' do
-        if !empty_fields?(params)
-            @roaster = Roaster.find_or_initialize_by(name: normalize(params[:name]))
-            @roaster.save
-            erb :'/roasters/show'
+        @roaster = Roaster.find_or_initialize_by(name: normalize(params[:name]))
+        @roaster.save
+        flash[:message] = "You successfully posted a new Coffee Roaster!"
+        redirect "/roasters/#{@roaster.slug}"
+    end
+
+    get '/roasters/:slug/edit' do
+        if logged_in?
+            @roaster = Roaster.find_by_slug(params[:slug])
+            @roasts = Coffee.roasts
+            erb :'/roasters/edit'
         else
-            redirect '/roasters/new'
+            redirect '/login'
         end
+    end
+
+    patch '/roasters/:slug' do
+       @roaster = Roaster.find_by_slug(params[:slug])
+        coffee = Coffee.find_or_initialize_by(name: normalize(params[:coffee][:name]), roast: params[:coffee][:roast])
+        @roaster.coffees << coffee 
+        flash[:message] = "You successfully added a Coffee to #{@roaster.name}!"
+        redirect "/roasters/#{@roaster.slug}"
     end
 
     get '/roasters/:slug' do 
