03b5f50e79d402b6438cf20c924408d96c7c0222
iamtash <natashachernis@gmail.com>
2019-07-09 00:34:41 -0500

    remove unnecessary checks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,8 @@ class UsersController < ApplicationController
    end
 
    post '/login' do
-    if empty_fields?(params)
-        flash[:login_error] = "Please fill in all fields."
-        redirect '/login'
-    else
         login(email: params[:email], password: params[:password])
         redirect '/cups'
-    end
-    
    end
 
    get '/signup' do
@@ -28,15 +22,10 @@ class UsersController < ApplicationController
    end
 
     post '/signup' do
-        if empty_fields?(params)
-            flash[:signup_error] = "Please fill in all fields."
-            redirect '/signup'
-        else
-            user = User.create(params)
-            login(email: user.email, password: user.password)
-            flash[:account_created] = "You successfully created an account!"
-            redirect '/cups'
-        end
+        user = User.create(params)
+        login(email: user.email, password: user.password)
+        flash[:account_created] = "You successfully created an account!"
+        redirect '/cups'
     end
 
     get '/logout' do
